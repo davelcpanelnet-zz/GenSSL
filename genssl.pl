789aa2e6089e93de2053fb489752fb3ede77985a
davelcpanelnet <davel@cpanel.net>
2016-05-19 19:59:56 -0500

    Getopt::Long added and various restructuring.

--- genssl.pl
+++ genssl.pl
@@ -5,50 +5,7 @@
 #   that won't expire for 10 years.  This is a task I repeatedly did while testing SSL functionality
 #   so I finally decided to spit out a dead simple script to accomlpish the task.
 
-my ($domain) = @ARGV;
-my $date = `date +%F`;
-chomp($date);
-my $tmp  = "/tmp/genssl1-$date";
-my $keep = '';
-my $ssl  = {};
-
-_help() unless $domain =~ /(\w+\.)+\w+/;
-
-mkdir $tmp unless -d $tmp;
-
-foreach (@ARGV) {
-    $keep = 1 if $_ =~ /--keep|-k/;
-    next;
-}
-
-system("openssl genrsa -des3 -passout pass:x -out $tmp/$domain.pass.key 2048");
-system("openssl rsa -passin pass:x -in $tmp/$domain.pass.key -out $tmp/$domain.key");
-system("openssl req -new -key $tmp/$domain.key -out $tmp/$domain.csr");
-system("openssl x509 -req -days 3650 -in $tmp/$domain.csr -signkey $tmp/$domain.key -out $tmp/$domain.crt");
-
-# Read Files into Variables.
-@{ $ssl->{'key'} } = _get_file_content("$tmp/$domain.key");
-@{ $ssl->{'crt'} } = _get_file_content("$tmp/$domain.crt");
-
-if ( $ssl->{'crt'} && $ssl->{'key'} ) {
-## DISPLAY CONTENTS
-    _print( @{ $ssl->{'crt'} }, $domain );
-    _print( @{ $ssl->{'key'} }, $domain );
-    print "\n";
-}
-else {
-    die "The $domain.csr or $domain.key files were unable to be created.";
-}
-
-## DESTROY
-if ( !$keep ) {
-    system("rm -Rf $tmp");
-}
-else {
-    print "The following files have been preserved:\n\n";
-    print "$tmp/$domain.key\n";
-    print "$tmp/$domain.crt\n\n";
-}
+use Getopt::Long;
 
 sub _get_file_content {
     my $file = shift;
@@ -64,11 +21,14 @@ sub _get_file_content {
 
 sub _help {
     print <<"HELP";
-Usage $0 domain.name
+Usage $0 --domain domain.name
 
 This is a simple script that will generate a self-signed certificate and output the *\.[crt|key] files for the domain.
 
-    Example: $0 www.cpanel.net
+    Example: $0 --domain www.cpanel.net --keep
+
+--domain
+    The domain an SSL should be generated for.
 
 --keep|-k 
     This option will prevent the removal of the CRT and KEY files.  By default these will be in /tmp/genssl-mmddyyyy/.
@@ -78,9 +38,64 @@ HELP
 }
 
 sub _print {
-    my ( @content, $domain ) = @_;
-    print "\nNow Displaying CRT for $domain\n\n";
-    foreach (@content) {
-        print $_;
+    my ( $content, $domain ) = @_;
+    if ( @$content && $domain ) {
+        print "\nNow Displaying CRT for $domain\n\n";
+        foreach (@$content) {
+            print $_;
+        }
     }
 }
+
+sub create_ssl {
+    my ( $ssl, $tmp, $domain ) = @_;
+    system("openssl genrsa -des3 -passout pass:x -out $tmp/$domain.pass.key 2048");
+    system("openssl rsa -passin pass:x -in $tmp/$domain.pass.key -out $tmp/$domain.key");
+    system("openssl req -new -key $tmp/$domain.key -out $tmp/$domain.csr");
+    system("openssl x509 -req -days 3650 -in $tmp/$domain.csr -signkey $tmp/$domain.key -out $tmp/$domain.crt");
+
+    # Read Files into Variables.
+    @{ $ssl->{'key'} } = _get_file_content("$tmp/$domain.key");
+    @{ $ssl->{'crt'} } = _get_file_content("$tmp/$domain.crt");
+    _print( \@{ $ssl->{'crt'} }, $domain );
+    _print( \@{ $ssl->{'key'} }, $domain );
+    return 1 if -e "$tmp/$domain.crt" && "$tmp/$domain.key";
+    return;
+}
+
+sub script {
+    my (@argv) = @_;
+    my ( $domain, $help, $keep );
+    my $opts = Getopt::Long::GetOptionsFromArray(
+        \@argv,
+        'domain=s' => \$domain,
+        'help'     => \$help,
+        'keep|k'   => \$keep
+    ) or _help();
+
+    if ( defined $help || !defined $domain ) {
+        _help();
+    }
+
+    my $date = `date +%F`;
+    chomp($date);
+    my $tmp = "/tmp/genssl1-$date";
+    my $ssl = {};
+
+    mkdir $tmp unless -d $tmp;
+    die "The $domain.csr or $domain.key files were unable to be created." unless create_ssl( $ssl, $tmp, $domain );
+    print "\n";
+## DESTROY
+    if ( defined $keep ) {
+        print "The following files have been preserved:\n\n";
+        print "$tmp/$domain.key\n";
+        print "$tmp/$domain.crt\n\n";
+    }
+    else {
+        system("rm -Rf $tmp");
+    }
+}
+
+exit( script(@ARGV) ) unless caller();
+
+1;
